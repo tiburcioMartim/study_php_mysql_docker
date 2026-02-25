7230bffd09c303d8932ac5c40232f97214c37390
Martim Tiburcio <tiburciomartim@gmail.com>
2026-02-25 12:32:19 -0300

    exercicio(variaveis-ambiente): Retornando a um ponto sem as variáveis para poder praticar a implementação.

--- app/conexao.php
+++ app/conexao.php
@@ -1,14 +1,10 @@
 <?php
-    require_once __DIR__ . '/vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-    $hostname = $_ENV['DB_HOST'];
-    $username = $_ENV['DB_USER'];
-    $password = $_ENV['DB_PASS'];
-    $database = $_ENV['DB_BANCO'];
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'projeto';
 
     try {
         $conn = new mysqli($hostname, $username, $password, $database);
